9685dd4f6b97607a776d26bb523f9c7787e63303
evans-nyang <nyanggithub@gmail.com>
2022-12-23 15:39:03 +0300

    add custom settings

--- opbot/spiders/jumia.py
+++ opbot/spiders/jumia.py
@@ -5,24 +5,31 @@ import scrapy
 
 class JumiaSpider(scrapy.Spider):
     name = "jumia"
+    custom_settings = {
+        # "LOG_FILE": "logs/jumia.log",
+        "LOG_LEVEL": "INFO",
+        "FEED_FORMAT": "json",
+        # "FEED_URI": f"../datasets/base/{name}/{scrapping_date}-%(batch_id)01d.json",
+        "FEED_EXPORT_BATCH_ITEM_COUNT": 100
+    }
     start_urls = ["https://www.jumia.co.ke/cooking-oil/"]
-    # store scraped items to a json file in dataset directory
-    # custom_settings = {"FEED_FORMAT": "json", "FEED_URI": "../dataset/phones.json"}
 
     def parse(self, response):
         """
-            Find metadata information and add to dictionary
-            Args: 
-              response
-            Returns: 
-              dict: nested dictionaries with indices as the keys
-            Notes:
-              See https://docs.scrapy.org/en/latest/
-              for more info
+        Find metadata information and add to dictionary
+        Args:
+          response
+        Returns:
+          dict: nested dictionaries with indices as the keys
+        Notes:
+          See https://docs.scrapy.org/en/latest/
+          for more info
         """
         try:
             print("\nCrawling through jumia")
-            articles = response.xpath("/html/body/div[1]/main/div[2]/div[3]/section/div[1]/article[@class='prd _fb col c-prd']")
+            articles = response.xpath(
+                "/html/body/div[1]/main/div[2]/div[3]/section/div[1]/article[@class='prd _fb col c-prd']"
+            )
             newdata = {}
             for i, art in enumerate(articles):
                 if i < 250:
@@ -34,7 +41,7 @@ class JumiaSpider(scrapy.Spider):
             raise err
 
         else:
-            pass
+            print("This is a new page..")
             next_page = (
                 response.xpath(
                     "/html/body/div[1]/main/div[2]/div[3]/section/div[2]/a[6]"
@@ -47,11 +54,11 @@ class JumiaSpider(scrapy.Spider):
 
     def parse_result(self, art) -> dict:
         """
-            update crawling time and metadata into dictionary
-            Args: 
-              art(dict)
-            Returns: 
-              dict
+        update crawling time and metadata into dictionary
+        Args:
+          art(dict)
+        Returns:
+          dict
         """
         data = {}
         core = art.xpath(".//a[@class='core']")
@@ -62,27 +69,47 @@ class JumiaSpider(scrapy.Spider):
         data["href"] = self.__safe_parsing(core.xpath("@href").get())
         data["data-id"] = self.__safe_parsing(core.xpath("@data-id").get())
         data["brand"] = self.__safe_parsing(core.xpath("@data-brand").get())
-        data["name2"] = self.__safe_parsing(core.xpath(".//div[@class='info']/h3/text()").get())
-        data["price"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='prc']/text()").get())
-        data["old_price"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='s-prc-w']/div/text()").get())
-        data["discount"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='s-prc-w']/div[@class='bdg _dsct _sm']/text()").get())
-        data["votes"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='rev']/text()").get())
-        data["stars"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='rev']/div[@class='stars _s']/text()").get())
-        data["image_url"] = self.__safe_parsing(core.xpath(".//div[@class='img-c']/img[@class='img']/@data-src").get())
-        data["official_store"] = self.__safe_parsing(core.xpath(".//div[@class='info']/div[@class='bdg _mall _xs']/text()").get())
+        data["name2"] = self.__safe_parsing(
+            core.xpath(".//div[@class='info']/h3/text()").get()
+        )
+        data["price"] = self.__safe_parsing(
+            core.xpath(".//div[@class='info']/div[@class='prc']/text()").get()
+        )
+        data["old_price"] = self.__safe_parsing(
+            core.xpath(".//div[@class='info']/div[@class='s-prc-w']/div/text()").get()
+        )
+        data["discount"] = self.__safe_parsing(
+            core.xpath(
+                ".//div[@class='info']/div[@class='s-prc-w']/div[@class='bdg _dsct _sm']/text()"
+            ).get()
+        )
+        data["votes"] = self.__safe_parsing(
+            core.xpath(".//div[@class='info']/div[@class='rev']/text()").get()
+        )
+        data["stars"] = self.__safe_parsing(
+            core.xpath(
+                ".//div[@class='info']/div[@class='rev']/div[@class='stars _s']/text()"
+            ).get()
+        )
+        data["image_url"] = self.__safe_parsing(
+            core.xpath(".//div[@class='img-c']/img[@class='img']/@data-src").get()
+        )
+        data["official_store"] = self.__safe_parsing(
+            core.xpath(".//div[@class='info']/div[@class='bdg _mall _xs']/text()").get()
+        )
 
         return data
 
     def __safe_parsing(self, parsing) -> str:
         """
-            assert if parsing arg is of type str, extract str from selector item if not
-            Args:
-              parsing(str): article from the crawler
-            Returns:
-              str: data from tag as a string
-              none: no values retrieved from arg
-            Raises:
-              valueError: if instance is not str or Selector
+        assert if parsing arg is of type str, extract str from selector item if not
+        Args:
+          parsing(str): article from the crawler
+        Returns:
+          str: data from tag as a string
+          none: no values retrieved from arg
+        Raises:
+          valueError: if instance is not str or Selector
         """
         try:
             if isinstance(parsing, str):
