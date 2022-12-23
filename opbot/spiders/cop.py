00b68265ffd7061e2d145a924d7aed2c464cad69
evans-nyang <nyanggithub@gmail.com>
2022-12-23 15:00:10 +0300

    encapsulate scraper in a class

--- opbot/spiders/cop.py
+++ opbot/spiders/cop.py
@@ -2,12 +2,15 @@ import datetime
 import re
 import time
 
+import bs4
 import scrapy
 from bs4 import BeautifulSoup
+from scrapy.http import HtmlResponse
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from scrapy.utils.project import get_project_settings
 
 
 class CopiaSpider(scrapy.Spider):
@@ -25,74 +28,138 @@ class CopiaSpider(scrapy.Spider):
     start_urls = [
         "https://copia.co.ke/product-category/all/saleable/foodstuff/cooking-oils/"
     ]
-    # Instantiate options
-    # option = webdriver.ChromeOptions()
-    option = Options()
+    
+    option = Options() #instantiate options
     option.add_argument("--start-maximized") #open Browser in maximized mode
     option.add_argument("--no-sandbox") #bypass OS security model
     option.add_argument("--disable-dev-shm-usage") #overcome limited resource problems
     option.add_experimental_option("excludeSwitches", ["enable-automation"])
     option.add_experimental_option('useAutomationExtension', False)
 
-    # def parse(self, response):
-    #     yield scrapy.Request(url=self.start_urls[0], callback=self.connection)
-
-    def scroll(self, driver):
-        scroll_pause_time = 1
-        # get height of the screen 
-        screen_height = driver.execute_script("return window.screen.height;")
-        i = 1
-
-        # scroll infinitely to obtain items
-        while True:
-            # scroll one screen height at a time 
-            driver.execute_script("window.scrollTo(0, {screen_height}*{i});".format(screen_height=screen_height, i=i))
-            i += 1
-            time.sleep(scroll_pause_time)
-            # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
-            scroll_height = driver.execute_script("return document.body.scrollHeight;")
-            # Break the loop when the height we need to scroll to is larger than the total scroll height
-            if (screen_height) * i > scroll_height:
-                break
-
-    def connection(self, response):
+    def parse(self, response):
+        """
+            instantiate the webdriver and load url
+        """
         try:
-            # Load the HTML page
+            url = 'https://copia.co.ke/product-category/all/saleable/foodstuff/cooking-oils/'
             driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=self.option)
         except Exception as err:
             print("Encountered an exception!!")
             raise err
         else:
-            driver.get('https://copia.co.ke/product-category/all/saleable/foodstuff/cooking-oils/')
+            driver.get(url)
             time.sleep(10) # delay to allow webpage to load
             self.scroll(driver)
+            # data = driver.page_source
+            # self.extractor(data)
+            yield scrapy.Request(url=url, callback=self.extractor)
+            # driver.quit()
 
+            # resp = HtmlResponse(url=url, body=data, encoding='utf-8')
+            # return resp
+
+    def scroll(self, driver):
+        """
+            handle infinite scroll of the pages one page at a time
+            Args:
+            driver(selenium.webdriver): engine to execute scripts
+        """
+        # driver = self.driver
+        scroll_pause_time = 1
+        # get height of the screen 
+        screen_height = driver.execute_script("return window.screen.height;")
+        pg = 1
+
+        # scroll infinitely to obtain items
+        while True:
+            # scroll one screen height at a time 
+            driver.execute_script("window.scrollTo(0, {screen_height}*{pg});".format(screen_height=screen_height, pg=pg))
+            pg += 1
+            time.sleep(scroll_pause_time)
+            # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+            scroll_height = driver.execute_script("return document.body.scrollHeight;")
+            # Break the loop when the height we need to scroll to is larger than the total scroll height
+            if (screen_height) * pg > scroll_height:
+                break
+
+    def extractor(self, response) -> dict:
+        """
+            update result from parsed items into dictionary
+            Args:
+            response 
+            Returns:
+            dict: nested dictionaries comprising data points with their indices as key-value pairs
+        """
+        try:
+            # pageSource = driver.find_element_by_xpath("//*").get_attribute("outerHTML")
+            # soup = BeautifulSoup(pageSource, 'lxml')
             # Parse processed webpage with BeautifulSoup
-            soup = BeautifulSoup(driver.page_source, 'lxml')
-
+            # soup = BeautifulSoup(self.driver.page_source, 'lxml
+            # data = response
+            # resp = HtmlResponse(url=self.start_urls[0], body=data, encoding='utf-8')
+            soup = BeautifulSoup(response, 'lxml')
+            
+            newitems = {}
             mainPage = soup.find(id="main")
             containerPage = mainPage.find("div", {"class":"shop-container"})
             products = containerPage.findAll("div", {"class":"product-small box"})
-            textContainer = containerPage.findAll("div", {"class":"box-text box-text-products"})
-            nametag = containerPage.findAll("p", {"class":re.compile("^name.+")})
-            # category = [art.find("p").text for art in textContainer]
-            # link1 = [art.find("a").attrs["href"] for art in products]
-            # img_url = [art.find("img").attrs["data-src"] for art in products]
-            
-            name = [art.text for art in nametag]
-            link = [art.find("a").attrs["href"] for art in nametag]
-            price = [art.find("bdi").text for art in textContainer]
-            dataID = [art.find("div", {"class":re.compile("^add-to-cart.+")}).find("a").attrs["data-product_id"] for art in textContainer]
-            sku = [art.find("div", {"class":re.compile("^add-to-cart.+")}).find("a").attrs["data-product_sku"] for art in textContainer]
-            quantity = [art.find("div", {"class":re.compile("^add-to-cart.+")}).find("a").attrs["data-quantity"] for art in textContainer]
-            # print(f"This product: {name}-({link}) for {price}")
-            # print(len(nametag))
-            # print(nametag)
-            
-            
-            # print(f"Found {len(link)} items \n{link} || {img_url}")
-            # print(f"Found {len(img_url)} items \n{img_url}")
-            print(f"Found {len(name)} items.")
-        finally:
-            # exit 
-            driver.quit()
+            print(f"This is the number of items : {len(products)}")
+            for i, obj in enumerate(products):
+                if i < 250:
+                    newitems.update({i: self.parse_result(obj)})
+            # yield newitems
+            # print(newitems)
+            return newitems
+        except AttributeError:
+            print("Unknown or unexpected variable")
+            raise 
+        except Exception as err:
+            print("Exception occurred!!")
+            raise err
+
+    def parse_result(self, obj) -> dict:
+        """
+            parse soup objects and extract data points
+            Args:
+            obj(tag): bs4 element to be parsed
+            Returns:
+            dict: data points as key-value pairs
+        """
+        items = {}
+        textContainer = obj.find("div", {"class":"box-text box-text-products"})
+        nameTag = obj.find("p", {"class":re.compile("^name.+")})
+        textBox = textContainer.find("div", {"class":re.compile("^add-to-cart.+")}).find("a")
+        # items["link1"] = self.__safe_parsing(obj.find("a").attrs["href"])
+        items["category"] = self.__safe_parsing(textContainer.find("p").get_text())
+        items["img_url"] = self.__safe_parsing(obj.find("img").attrs["data-src"])
+        items["name"] = self.__safe_parsing(nameTag.text)
+        items["link"] = self.__safe_parsing(nameTag.find("a").attrs["href"])
+        items["price"] = self.__safe_parsing(textContainer.find("bdi").text)
+        items["dataID"] = self.__safe_parsing(textBox.attrs["data-product_id"])
+        items["sku"] = self.__safe_parsing(textBox.attrs["data-product_sku"])
+        items["quantity"] = self.__safe_parsing(textBox.attrs["data-quantity"])
+
+        return items
+
+    def __safe_parsing(self, parsing) -> str:
+        """
+            assert if parsing arg is of type str, extract str from selector item if not
+            Args:
+            parsing(str): article from the crawler
+            Returns:
+            str: data from tag as a string
+            none: no values retrieved from arg
+            Raises:
+            valueError: if instance is not str or Selector
+        """
+        try:
+            if isinstance(parsing, str):
+                return parsing
+            elif isinstance(parsing, bs4.element.NavigableString):
+                return parsing.strip()
+            elif isinstance(parsing, scrapy.Selector):
+                return parsing.get()
+        except Exception:
+            return None
+
+    
