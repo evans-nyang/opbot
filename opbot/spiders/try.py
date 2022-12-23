b0e48c1ac15df142bd25a6ea0465e0b71d9759a6
evans-nyang <nyanggithub@gmail.com>
2022-12-23 14:59:01 +0300

    update template

--- opbot/spiders/try.py
+++ opbot/spiders/try.py
@@ -19,6 +19,47 @@ option.add_argument("--disable-dev-shm-usage") #overcome limited resource proble
 option.add_experimental_option("excludeSwitches", ["enable-automation"])
 option.add_experimental_option('useAutomationExtension', False)
 
+def connection():
+    """
+        instantiate the webdriver and load url
+    """
+    try:
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
+    except Exception as err:
+        print("Encountered an exception!!")
+        raise err
+    else:
+        driver.get('https://copia.co.ke/product-category/all/saleable/foodstuff/cooking-oils/')
+        time.sleep(10) # delay to allow webpage to load
+        scroll(driver)
+
+        parse(driver)
+    finally:
+        driver.quit()
+
+def scroll(driver):
+    """
+        handle infinite scroll of the pages one page at a time
+        Args:
+          driver(selenium.webdriver): engine to execute scripts
+    """
+    scroll_pause_time = 1
+    # get height of the screen 
+    screen_height = driver.execute_script("return window.screen.height;")
+    pg = 1
+
+    # scroll infinitely to obtain items
+    while True:
+        # scroll one screen height at a time 
+        driver.execute_script("window.scrollTo(0, {screen_height}*{pg});".format(screen_height=screen_height, pg=pg))
+        pg += 1
+        time.sleep(scroll_pause_time)
+        # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
+        scroll_height = driver.execute_script("return document.body.scrollHeight;")
+        # Break the loop when the height we need to scroll to is larger than the total scroll height
+        if (screen_height) * pg > scroll_height:
+            break
+
 def parse(driver) -> dict:
     """
         update result from parsed items into dictionary
@@ -90,48 +131,7 @@ def __safe_parsing(parsing) -> str:
             return parsing.get()
     except Exception:
         return None
-
-def scroll(driver):
-    """
-        handle infinite scroll of the pages one page at a time
-        Args:
-          driver(selenium.webdriver): engine to execute scripts
-    """
-    scroll_pause_time = 1
-    # get height of the screen 
-    screen_height = driver.execute_script("return window.screen.height;")
-    pg = 1
-
-    # scroll infinitely to obtain items
-    while True:
-        # scroll one screen height at a time 
-        driver.execute_script("window.scrollTo(0, {screen_height}*{pg});".format(screen_height=screen_height, pg=pg))
-        pg += 1
-        time.sleep(scroll_pause_time)
-        # update scroll height each time after scrolled, as the scroll height can change after we scrolled the page
-        scroll_height = driver.execute_script("return document.body.scrollHeight;")
-        # Break the loop when the height we need to scroll to is larger than the total scroll height
-        if (screen_height) * pg > scroll_height:
-            break
-
-def connection():
-    """
-        instantiate the webdriver and load url
-    """
-    try:
-        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()),options=option)
-    except Exception as err:
-        print("Encountered an exception!!")
-        raise err
-    else:
-        driver.get('https://copia.co.ke/product-category/all/saleable/foodstuff/cooking-oils/')
-        time.sleep(10) # delay to allow webpage to load
-        scroll(driver)
-
-        parse(driver)
-    finally:
-        driver.quit()
-
+        
 
 if __name__ == "__main__":
     connection()
